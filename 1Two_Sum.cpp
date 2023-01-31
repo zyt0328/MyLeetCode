4a3f3e6073941d4eecd02adbd34a64a74b72bc8f
zyt0328 <71568759+zyt0328@users.noreply.github.com>
2023-01-30 23:29:40 -0800

    Update 1Two_Sum.cpp

--- 1Two_Sum.cpp
+++ 1Two_Sum.cpp
@@ -17,3 +17,21 @@ public:
 };
 
 //Use haspmap
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::map<int, int> hashmap;
+        vector<int> res;
+        for(int i = 0; i< nums.size(); i++){
+            int  complement= target - nums[i];
+            if (hashmap.find(complement) == hashmap.end()){
+                hashmap[nums[i]] = i;
+            }else{
+                res.push_back(hashmap[complement]);
+                res.push_back(i);
+                return res;
+            }
+        }
+        return res;
+    }
+};
