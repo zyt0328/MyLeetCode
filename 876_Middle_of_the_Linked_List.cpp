0acf9088c1c192c0a28f3882a40479feffcb4d54
zyt0328 <71568759+zyt0328@users.noreply.github.com>
2023-02-05 22:05:22 -0800

    Create 876_Middle_of_the_Linked_List.cpp

---
+++ 876_Middle_of_the_Linked_List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(NULL == head || NULL == head->next){
+            return head;
+        }
+        ListNode* p1 = head;
+        ListNode* p2 = head;
+        while(p1 != NULL && p1->next != NULL){
+            p1 = p1->next->next;
+            p2 = p2->next;
+        }
+
+        
+        return p2;
+    }
+};
