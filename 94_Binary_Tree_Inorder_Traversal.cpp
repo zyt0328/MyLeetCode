f375dd94cf0ce3b794bb79230837b55b96c4fc73
zyt0328 <71568759+zyt0328@users.noreply.github.com>
2023-02-07 21:30:50 -0800

    Create 94_Binary_Tree_Inorder_Traversal.cpp

---
+++ 94_Binary_Tree_Inorder_Traversal.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(NULL == root){
+            vector<int> val1;
+            return val1;
+        }
+        if(NULL == root->left && NULL == root->right){
+            vector<int> val1;
+            val1.push_back(root->val);
+            return val1;
+        }else if(NULL == root->left){
+            vector<int> val1;
+            vector<int> val2 = inorderTraversal(root->right); 
+            val1.push_back(root->val);
+            val1.insert(val1.end(),val2.begin(),val2.end());
+            return val1;
+        }else if( NULL == root->right){
+            vector<int> val1;
+            vector<int> val2 = inorderTraversal(root->left);
+            val1.insert(val1.end(),val2.begin(),val2.end());
+            val1.push_back(root->val);
+            return val1;
+        }else{
+            vector<int> val1;
+            vector<int> val2 = inorderTraversal(root->left);
+            vector<int> val3 = inorderTraversal(root->right);
+            val1.insert(val1.end(),val2.begin(),val2.end());
+            val1.push_back(root->val);
+            val1.insert(val1.end(),val3.begin(),val3.end());
+            return val1;
+        }
+    }
+};
